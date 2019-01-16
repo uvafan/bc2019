d3ef7bdd10c7a4526f235dd35c0265d7e3c1a7db
uvafan <eli.d.lifland@gmail.com>
2019-01-15 19:00:07 -0500

    sprint pathing

---
+++ v10_sprint/params.js
@@ -0,0 +1,18 @@
+import {SPECS} from 'battlecode'; 
+
+export var DEBUG=true;
+
+//defense stuff
+export var DEFENDERS_ATTACK_ROUND=700;
+export var DEFENSIVE_PROPHET_DISTANCE=5;
+export var DEFENSIVE_PREACHER_DISTANCE=5;
+
+//desired unit comps: [castle,church,pilgrim,crusader,prophet,preacher]
+//export var BALANCED_UNIT_COMP=[0,0,2,5,0,0];
+
+//nav weights; movement vs. fuel efficiency vs. splash resistance
+export var DEF_NAV_WEIGHTS=[2,1,0];
+export var ATT_NAV_WEIGHTS=[10,1,0];
+export var PILGRIM_NAV_WEIGHTS=[10,1,0];
+
+export var STOP_PENALTY=3;
---
+++ v10_sprint/unit.js
@@ -0,0 +1,137 @@
+import * as params from 'params.js';
+import {SPECS} from 'battlecode'; 
+export class Unit{
+    constructor(rc){
+        this.mapSize = rc.map.length;
+        this.rc = rc;
+        this.me = rc.me;
+        this.adjMoves = [[-1,0],[1,0],[0,-1],[0,1]];
+        this.adjDiagMoves = [[-1,0],[1,0],[0,-1],[0,1],[-1,-1],[-1,1],[1,-1],[1,1]];
+        this.visRobotMap = this.rc.getVisibleRobotMap();
+    }
+    
+    getDxDyWithin(min,max){
+        var ret = [];
+        for(var dx=0;dx*dx<=max;dx++){
+            for(var dy=0;dy*dy<=max;dy++){
+                if(dx*dx+dy*dy<=max&&dx*dx+dy*dy>=min){
+                    ret.push([dx,dy]);
+                    if(dx>0||dy>0)
+                        ret.push([-dx,-dy]);
+                    if(dx>0 && dy>0){
+                        ret.push([-dx,dy]);
+                        ret.push([dx,-dy]);
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    turn(rc){
+        this.updateInfo(rc);
+        if(this.me.unit!=SPECS['Castle'])
+            this.sendCastleTalk();
+    } 
+
+    updateInfo(rc){
+        this.rc = rc;
+        this.me = rc.me;
+        this.visRobotMap = this.rc.getVisibleRobotMap();
+    }
+
+    sendCastleTalk(){
+        if((this.me.turn+this.offset)%2==0){
+            this.rc.castleTalk(this.me.unit+1);
+        }
+        else if((this.me.turn+this.offset)%2==1&&this.castleDead){
+            this.rc.castleTalk(1);
+            this.castleDead=false;
+        }
+    }
+
+    getBroadcastFromLoc(loc){
+        return loc[0]*64+loc[1];
+    }
+
+    getLocFromBroadcast(b){
+        return [Math.floor(b/64),b%64];
+    }
+
+    //0 is horiz, 1 is vert
+    determineSymmetry(){
+        for(var x=0;x<this.mapSize;x++){
+            for(var y=0;y<=this.mapSize/2;y++){
+                if(this.rc.map[y][x]!=this.rc.map[this.mapSize-y-1][x])
+                    return 1;
+            }
+        }
+        return 0;
+    }
+
+    log(s){
+        if(!params.DEBUG)
+            return;
+        if(this.me.unit==SPECS['CASTLE'])
+            this.rc.log('Round '+this.me.turn+': '+s);
+        else
+            this.rc.log(s);
+    }
+
+    reflect(x,y){
+        if(!this.symmetry)
+            this.symmetry = this.determineSymmetry();
+        if(this.symmetry==1){
+            return [this.mapSize-x-1,y];
+        }
+        else{
+            return [x,this.mapSize-y-1];
+        }
+    }
+    
+    offMap(x,y){
+        return x<0||x>=this.mapSize||y<0||y>=this.mapSize;
+    }
+
+    //on map and is passable
+    isPassable(x,y){
+        return !this.offMap(x,y) && this.rc.map[y][x];
+    }
+
+    //can walk there on this turn
+    isWalkable(x,y){
+        return this.isPassable(x,y) && (this.visRobotMap[y][x]<=0 || this.visRobotMap[y][x]==this.me.id);
+    }
+
+    isSafe(x,y){
+        var visRobots = this.rc.getVisibleRobots();
+        for(var i=0;i<visRobots.length;i++){
+            var r=visRobots[i];
+            if(r.x==null || r.team==this.me.team)
+                continue;
+            var d = this.distBtwnP(x,y,r.x,r.y);
+            var attack_rad = SPECS.UNITS[r.unit]['ATTACK_RADIUS'];
+            if(!attack_rad||d<attack_rad[0]||d>attack_rad[1])
+                continue;
+            return false;
+        }
+        return true;
+    }
+
+    distBtwn(r0,r1){
+        return this.distBtwnP(r0.x,r0.y,r1.x,r1.y);
+    }
+
+    distBtwnP(x0,y0,x1,y1){
+        return (x0-x1)*(x0-x1)+(y0-y1)*(y0-y1);
+    }
+
+    manhattan(x0,y0,x1,y1){
+        return Math.abs(x1-x0)+Math.abs(y1-y0);
+    }
+
+    locsEqual(l0,l1){
+        return l0[0]==l1[0]&&l0[1]==l1[1];
+    }
+
+}
