81e96de2dd41e1c3ccdc3e17fc764da9654267a6
uvafan <eli.d.lifland@gmail.com>
2019-01-26 19:46:50 -0500

    nothing

--- cur/objective.js
+++ cur/objective.js
@@ -162,7 +162,7 @@ export class defendCastle extends Objective {
         var enemiesInSight = this.th.getEnemiesInSight();
         var attackersInSight = this.th.getAttackersInSight();
         var dangerScore = Math.max((attackersInSight.length*2-numDefenders)*100,((enemiesInSight.length&&numDefenders==0)?100:0));
-        return Math.max(10-numDefenders/4-this.manDistFromEnemy/8,Math.max(dangerScore,(this.isCastle?5:1)))+((damaged&&this.isCastle)&&numDefenders<10?30:0);
+        return Math.max(10-numDefenders/4-this.manDistFromEnemy/8,Math.max(dangerScore,(this.isCastle?5:1)))+((damaged&&this.isCastle&&numDefenders<10)?30:0);
     }
 
     initializeDefenseSpots(){
