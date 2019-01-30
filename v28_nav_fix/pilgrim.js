07ebdfd65dc3bfb140f96440504e701146979bd6
uvafan <eli.d.lifland@gmail.com>
2019-01-30 15:54:27 -0500

    v28

---
+++ v28_nav_fix/pilgrim.js
@@ -0,0 +1,273 @@
+import {SPECS} from 'battlecode';
+import {Robot} from 'myrobot.js';
+import * as params from 'params.js';
+
+export class Pilgrim extends Robot{
+    constructor(rc){
+        super(rc);
+        this.getFirstTarget();
+        //this.structDists = this.runBFS(this.structLoc,true);
+        this.locToMine = null;
+        this.doNothing=false;
+        this.updated=false;
+    }
+
+    updateDoNothing(){
+        var visRobots = this.rc.getVisibleRobots();
+        for(var i=0;i<visRobots.length;i++){
+            var r=visRobots[i];
+            if(r.x!=null && r.unit == SPECS['CHURCH'] && r.team==this.me.team){
+                if(this.distBtwnP(r.x,r.y,this.target[0],this.target[1])<=8){
+                    this.churchBuilt=true;
+                    this.doNothing=true;
+                    this.buildingChurch=false;
+                    return this.moveToDoNothingLocation();
+                }
+            }
+        }
+    }
+
+    moveToDoNothingLocation(){
+        var bestScore= Number.MIN_SAFE_INTEGER;
+        var bestMove = [0,0];
+        for(var i=0;i<this.possibleMoves.length;i++){
+            var move = this.possibleMoves[i];
+            var nx = this.me.x+move[0];
+            var ny = this.me.y+move[1];
+            var score = Number.MIN_SAFE_INTEGER;
+            if(this.isWalkable(nx,ny)&&!this.rc.karbonite_map[ny][nx]&&!this.rc.fuel_map[ny][nx]&&((nx+ny)%2==0)){
+                score = -move[0]*move[0]-move[1]*move[1];
+            }
+            if(score>bestScore){
+                bestScore=score;
+                bestMove=move;
+            }
+        }
+        return this.rc.move(bestMove[0],bestMove[1]);
+    }
+
+    updateChurchLoc(){
+        var options = this.getDxDyWithin(1,16);
+        for(var i=0;i<options.length;i++){
+            var nx = this.target[0]+options[i][0]
+            var ny = this.target[1]+options[i][1]
+            if(this.isWalkable(nx,ny)&&this.isSafe(nx,ny)&&!(this.rc.karbonite_map[ny][nx]||
+                this.rc.fuel_map[ny][nx])){
+                this.updateTarget([nx,ny]);
+                this.updated=true;
+                break;
+            }
+        }
+    }
+
+    turn(rc){
+        super.turn(rc);
+        if(this.buildingChurch){
+            var dn = this.updateDoNothing();
+            if(dn)
+                return dn;
+        }
+        if(this.doNothing){
+            return null;
+        }
+        this.karbNeeded = (this.rc.karbonite*params.FUEL_KARB_RATIO<this.rc.fuel?1:0);
+        if(this.me.turn%10 == 5){
+            this.structDists = this.runBFS(this.structLoc,true);
+        }
+        if(this.buildingChurch){
+            if(!this.isSafe(this.target[0],this.target[1])&&!this.updated)
+                this.updateChurchLoc();
+            this.log('trying to bc at '+this.target);
+            var distToT = this.distBtwnP(this.target[0],this.target[1],this.me.x,this.me.y);
+            if(distToT<=2){
+                if(this.shouldBuildChurch()){
+                    var build = this.buildChurch();
+                    var locToMine = this.getFirstMiningLoc();
+                    if(build){
+                        this.churchBuilt=true;
+                        this.buildingChurch=false;
+                        //this.log('gonna mine '+locToMine);
+                        this.updateTarget(locToMine);
+                    }
+                    return build;
+                }
+                else{
+                    if(!this.locToMine||this.isPartiallyFull()){
+                        this.locToMine = this.getWaitingMiningLoc();
+                        if(this.locToMine){
+                            this.mineDists = this.runBFS(this.locToMine,false);
+                        }
+                    }
+                    if(this.locToMine&&this.rc.fuel>0&&this.locsEqual(this.locToMine,[this.me.x,this.me.y])&&!this.isFullyFull()){
+                        return this.rc.mine();
+                    }
+                    else if(this.locToMine){
+                        return this.navTo(this.mineDists,this.locToMine,params.PILGRIM_NAV_WEIGHTS,true,true);
+                    }
+                }
+            }
+            else{
+                return this.navTo(this.targetDists,this.target,params.PILGRIM_NAV_WEIGHTS,true,false);
+            }
+        }
+        else if(this.isFull()){
+            var giveToDefenseUnit = this.giveBack(this.structLoc);
+            if(giveToDefenseUnit)
+                return giveToDefenseUnit;
+            return this.navTo(this.structDists,this.structLoc,params.PILGRIM_NAV_WEIGHTS,true,true);
+        }
+        else if(this.me.x==this.target[0]&&this.me.y==this.target[1]&&this.rc.fuel>0){
+            return this.rc.mine();
+        }
+        else{
+            return this.navTo(this.targetDists,this.target,params.PILGRIM_NAV_WEIGHTS,true,true);
+        }
+        return null;
+    }
+
+    shouldBuildChurch(){
+        var karbSave = params.MIN_KARB_SAVE;
+        var fuelSave = params.MIN_FUEL_SAVE;
+        var karbLeft = this.rc.karbonite-SPECS.UNITS[SPECS['CHURCH']].CONSTRUCTION_KARBONITE;
+        var fuelLeft = this.rc.fuel-SPECS.UNITS[SPECS['CHURCH']].CONSTRUCTION_FUEL;
+        var enemiesInSight = this.getEnemiesInSight();
+        if(enemiesInSight.length>0){
+            karbSave=0;
+            fuelSave=2;
+        }
+        return karbLeft>=karbSave && fuelLeft >= fuelSave;
+    }
+
+    buildChurch(){
+        var bestScore = Number.MIN_SAFE_INTEGER;
+        var theirOffset = (this.me.turn%2==0?1-this.offset:this.offset);
+        var dial = this.getClosestDial(this.me.turn+this.createdTurn);
+        var cast = this.getBroadcastFromLoc(this.structLoc)+(this.karbNeeded<<15)+(dial<<12);
+        for(var i=0;i<this.adjDiagMoves.length;i++){
+            var nx=this.me.x+this.adjDiagMoves[i][0];
+            var ny=this.me.y+this.adjDiagMoves[i][1];
+            var d=this.distBtwnP(nx,ny,this.target[0],this.target[1]);
+            var safe = this.isSafe(nx,ny);
+            var score = (safe?100:0)-d;
+            if(this.isWalkable(nx,ny)&&score>bestScore&&!this.rc.karbonite_map[ny][nx]&&!this.rc.fuel_map[ny][nx]){
+                this.structLoc = [nx,ny];
+                bestScore=score;
+            }
+        }
+        var dx = this.structLoc[0]-this.me.x;
+        var dy = this.structLoc[1]-this.me.y;
+        this.rc.signal(cast,dx*dx+dy*dy);
+        this.structDists = this.runBFS(this.structLoc,true);
+        return this.rc.buildUnit(SPECS['CHURCH'],dx,dy);
+    }
+
+    getFirstMiningLoc(){
+        var dist = [];
+        for(var x=0;x<this.mapSize;x++){
+            dist.push([]);
+            for(var y=0;y<this.mapSize;y++){
+                dist[x].push(-1);
+            }
+        }
+        var q = [[this.structLoc[0],this.structLoc[1]]];
+        while(q.length>0){
+            var u = q.shift();
+            var x = u[0];
+            var y = u[1];
+            for(var i=0;i<4;i++){
+                var move = this.adjMoves[i];
+                var nx=x+move[0];
+                var ny=y+move[1];
+                if(!this.isPassable(nx,ny)||dist[nx][ny]>-1)
+                    continue;
+                if(this.karbNeeded?this.rc.karbonite_map[ny][nx]:this.rc.fuel_map[ny][nx]){
+                    return [nx,ny];
+                }
+                dist[nx][ny]=dist[x][y]+1;
+                q.push([nx,ny]);
+            }
+        }
+    }
+
+    getWaitingMiningLoc(){
+        var karbPreferred = this.karbNeeded;
+        var karbFull = this.isKarbFull();
+        var fuelFull = this.isFuelFull();
+        var ret = null;
+        var bestScore = Number.MIN_SAFE_INTEGER;
+        for(var i=0;i<8;i++){
+            var offset = this.adjDiagMoves[i];
+            var loc = [this.target[0]+offset[0],this.target[1]+offset[1]];
+            if(this.isPassable(loc[0],loc[1])){
+                var score = Number.MIN_SAFE_INTEGER;
+                if(this.rc.karbonite_map[loc[1]][loc[0]]){
+                    if(!karbFull){
+                        score = (karbPreferred?100:0)-this.distBtwnP(this.me.x,this.me.y,loc[0],loc[1]);
+                    }
+                }
+                else if(this.rc.fuel_map[loc[1]][loc[0]]){
+                    if(!fuelFull){
+                        score = (karbPreferred?0:100)-this.distBtwnP(this.me.x,this.me.y,loc[0],loc[1]);
+                    }
+                }
+                if(score>bestScore){
+                    bestScore=score;
+                    ret=loc;
+                }
+            }
+        }
+        return ret;
+    }
+
+    getFirstTarget(){
+        var visRobots = this.rc.getVisibleRobots();
+        for(var i=0;i<visRobots.length;i++){
+            var r = visRobots[i];
+            if(r.team==this.me.team&&this.distBtwnP(r.x,r.y,this.me.x,this.me.y)<=2&&(r.unit==SPECS['CASTLE']||r.unit==SPECS['CHURCH'])){
+                this.offset = r.signal>>15;
+                var now = (r.signal&((1<<15)-1));
+                var turnDial = now>>12;
+                this.createdTurn = params.PILGRIM_TURN_ARRAY[turnDial];
+                var locb = (now&((1<<12)-1));
+                //this.log(locb);
+                this.structLoc = [r.x,r.y];
+                this.eloc = this.reflect(r.x,r.y);
+                var target = this.getLocFromBroadcast(locb);
+                this.log('my target is '+target+' from broadcast '+locb);
+                this.updateTarget(this.getLocFromBroadcast(locb));
+                if(!this.rc.karbonite_map[this.target[1]][this.target[0]]&&!this.rc.fuel_map[this.target[1]][this.target[0]]/*this.distBtwnP(this.structLoc[0],this.structLoc[1],this.target[0],this.target[1])>params.MINING_DISTANCE*/){
+                    this.buildingChurch=true;
+                }
+            }
+        }
+        //this.log('T '+this.target);
+        //this.log('SL '+this.structLoc);
+    }
+
+    isFull(){
+        return this.me.fuel==SPECS.UNITS[this.me.unit]['FUEL_CAPACITY'] ||
+               this.me.karbonite==SPECS.UNITS[this.me.unit]['KARBONITE_CAPACITY'];
+    }
+
+    isPartiallyFull(){
+        return this.me.fuel==SPECS.UNITS[this.me.unit]['FUEL_CAPACITY'] &&
+               this.me.karbonite<SPECS.UNITS[this.me.unit]['KARBONITE_CAPACITY'] ||
+               this.me.fuel<SPECS.UNITS[this.me.unit]['FUEL_CAPACITY'] &&
+               this.me.karbonite==SPECS.UNITS[this.me.unit]['KARBONITE_CAPACITY'];
+
+    }
+
+    isKarbFull(){
+        return this.me.karbonite==SPECS.UNITS[this.me.unit]['KARBONITE_CAPACITY'];
+    }
+
+    isFuelFull(){
+        return this.me.fuel==SPECS.UNITS[this.me.unit]['FUEL_CAPACITY'];
+    }
+
+    isFullyFull(){
+        return this.me.fuel==SPECS.UNITS[this.me.unit]['FUEL_CAPACITY'] &&
+               this.me.karbonite==SPECS.UNITS[this.me.unit]['KARBONITE_CAPACITY'];
+    }
+
+}
---
+++ v28_nav_fix/unit.js
@@ -0,0 +1,251 @@
+import * as params from 'params.js';
+import {SPECS} from 'battlecode';
+export class Unit{
+    constructor(rc){
+        this.mapSize = rc.map.length;
+        this.everDefended=true;
+        this.castleDead=false;
+        this.churchBuilt=false;
+        this.rc = rc;
+        this.me = rc.me;
+        this.lastTime=-1;
+        this.adjMoves = [[-1,0],[1,0],[0,-1],[0,1]];
+        this.adjDiagMoves = [[-1,0],[1,0],[0,-1],[0,1],[-1,-1],[-1,1],[1,-1],[1,1]];
+        this.visRobotMap = this.rc.getVisibleRobotMap();
+        this.attackRadius = SPECS.UNITS[this.me.unit]['ATTACK_RADIUS'];
+        this.attackCost = SPECS.UNITS[this.me.unit]['ATTACK_FUEL_COST'];
+        this.turnsBeforeBFS = 10;
+        this.ranBFS = 0;
+    }
+
+    getClosestDial(num){
+        var minD = Number.MAX_SAFE_INTEGER;
+        var ret=-1;
+        for(var i=7;i>-1;i--){
+            if(params.PILGRIM_TURN_ARRAY[i]<num)
+                return i;
+        }
+        return ret;
+    }
+
+    getDxDyWithin(min,max){
+        var ret = [];
+        for(var dx=0;dx*dx<=max;dx++){
+            for(var dy=0;dy*dy<=max;dy++){
+                if(dx*dx+dy*dy<=max&&dx*dx+dy*dy>=min){
+                    ret.push([dx,dy]);
+                    if(dx>0||dy>0)
+                        ret.push([-dx,-dy]);
+                    if(dx>0 && dy>0){
+                        ret.push([-dx,dy]);
+                        ret.push([dx,-dy]);
+                    }
+                }
+            }
+        }
+        ret.sort(function(a,b){return (a[0]*a[0] + a[1]*a[1] - b[0]*b[0] - b[1]*b[1])});
+        return ret;
+    }
+    getDxDyOddWithin(min,max){
+        var ret = [];
+        for(var dx=0;dx*dx<=max;dx++){
+            for(var dy=0;dy*dy<=max;dy++){
+                if(dx*dx+dy*dy<=max&&dx*dx+dy*dy>=min){
+                    if((dx+dy)%2 == 0){
+                        ret.push([dx,dy]);
+                        if(dx>0||dy>0)
+                            ret.push([-dx,-dy]);
+                        if(dx>0 && dy>0){
+                            ret.push([-dx,dy]);
+                            ret.push([dx,-dy]);
+                        }
+                    }
+                }
+            }
+        }
+        ret.sort(function(a,b){return Math.abs(a[0])+Math.abs(a[1]) - Math.abs(b[0]) - Math.abs(b[1])});
+        return ret;
+    }
+
+    turn(rc){
+        this.updateInfo(rc);
+        if(this.me.unit!=SPECS['Castle']||this.me.turn>2)
+            this.sendCastleTalk();
+    }
+
+    updateInfo(rc){
+        this.lastTime=-1;
+        this.rc = rc;
+        this.me = rc.me;
+        this.visRobotMap = this.rc.getVisibleRobotMap();
+    }
+
+    sendCastleTalk(){
+        var msg = 0;
+        if(this.me.turn==1){
+            msg=this.me.unit;
+        }
+        else if((this.me.turn+this.offset)%2==0){
+            msg=this.me.x;
+        }
+        else if((this.me.turn+this.offset)%2==1){
+            msg=this.me.y;
+        }
+        if((this.castleDead&&!this.everDefended)||this.churchBuilt){
+            msg+=(1<<6);
+            this.churchBuilt=false;
+            this.castleDead=false;
+        }
+        this.rc.castleTalk(msg);
+    }
+
+    getBroadcastFromLoc(loc){
+        return loc[0]*64+loc[1];
+    }
+
+    getLocFromBroadcast(b){
+        return [Math.floor(b/64),b%64];
+    }
+
+    //0 is horiz, 1 is vert
+    determineSymmetry(){
+        for(var x=0;x<this.mapSize;x++){
+            for(var y=0;y<=this.mapSize/2;y++){
+                if(this.rc.map[y][x]!=this.rc.map[this.mapSize-y-1][x])
+                    return 1;
+            }
+        }
+        return 0;
+    }
+
+    log(s){
+        if(!params.DEBUG)
+            return;
+        if(params.DEBUG_TIMING){
+            var t = new Date().getTime();
+            var timeTaken=0;
+            if(this.lastTime!=-1){
+                timeTaken=t-this.lastTime;
+            }
+            this.lastTime=t;
+            s+=' time taken since last log '+timeTaken+' ms.'
+        }
+        if(this.me.unit==SPECS['CASTLE'])
+            this.rc.log('Round '+this.me.turn+': '+s);
+        else
+            this.rc.log(s);
+    }
+
+    reflect(x,y){
+        if(!this.symmetry)
+            this.symmetry = this.determineSymmetry();
+        if(this.symmetry==1){
+            return [this.mapSize-x-1,y];
+        }
+        else{
+            return [x,this.mapSize-y-1];
+        }
+    }
+
+    offMap(x,y){
+        return x<0||x>=this.mapSize||y<0||y>=this.mapSize;
+    }
+
+    //on map and is passable
+    isPassable(x,y){
+        return !this.offMap(x,y) && this.rc.map[y][x];
+    }
+
+    //can walk there on this turn
+    isWalkable(x,y){
+        return this.isPassable(x,y) && (this.visRobotMap[y][x]<=0 || this.visRobotMap[y][x]==this.me.id);
+    }
+
+    isSafe(x,y){
+        var visRobots = this.rc.getVisibleRobots();
+        for(var i=0;i<visRobots.length;i++){
+            var r=visRobots[i];
+            if(r.x==null || r.team==this.me.team || r.unit==null)
+                continue;
+            var d = this.distBtwnP(x,y,r.x,r.y);
+            var attack_rad = SPECS.UNITS[r.unit]['ATTACK_RADIUS'];
+            if(!attack_rad||d<attack_rad[0]||d>attack_rad[1])
+                continue;
+            return false;
+        }
+        return true;
+    }
+
+    distBtwn(r0,r1){
+        return this.distBtwnP(r0.x,r0.y,r1.x,r1.y);
+    }
+
+    distBtwnP(x0,y0,x1,y1){
+        return (x0-x1)*(x0-x1)+(y0-y1)*(y0-y1);
+    }
+
+    manhattan(x0,y0,x1,y1){
+        return Math.abs(x1-x0)+Math.abs(y1-y0);
+    }
+
+    locsEqual(l0,l1){
+        return l0[0]==l1[0]&&l0[1]==l1[1];
+    }
+
+    attackEnemy(){
+        if(this.rc.fuel < this.attackCost)
+            return null;
+        var visRobots = this.rc.getVisibleRobots();
+        var th = this;
+        var ret = null;
+        var bestScore = Number.MIN_SAFE_INTEGER;
+        visRobots.forEach(function(r){
+            if(r.x == null || r.team==null || r.team==th.me.team)
+                return;
+            var distToR = th.distBtwn(th.me,r);
+            if(th.attackRadius[0]>distToR||th.attackRadius[1]<distToR)
+                return;
+            var score = params.ATTACK_PRIORITIES[r.unit]*100-distToR;
+            if(score>bestScore){
+                bestScore = score;
+                ret = th.rc.attack(r.x-th.me.x,r.y-th.me.y);
+            }
+        });
+        return ret;
+    }
+
+    getEnemiesInSight(){
+        var visRobots = this.rc.getVisibleRobots();
+        var ret= [];
+        for(var i=0;i<visRobots.length;i++){
+            var r = visRobots[i];
+            if(r.x!=null && r.unit != null && r.team!=this.me.team){
+                ret.push(r);
+            }
+        }
+        return ret;
+    }
+
+    getAttackersInSight(){
+        var visRobots = this.rc.getVisibleRobots();
+        var ret= [];
+        for(var i=0;i<visRobots.length;i++){
+            var r = visRobots[i];
+            if(r.x!=null && r.unit != null && r.team!=this.me.team&&r.unit!=SPECS['PILGRIM']){
+                ret.push(r);
+            }
+        }
+        return ret;
+    }
+
+    centroid(locs){
+        var xSum=0;
+        var ySum=0;
+        for(var i=0;i<locs.length;i++){
+            xSum+=locs[i][0];
+            ySum+=locs[i][1];
+        }
+        return [Math.round(xSum/locs.length),Math.round(ySum/locs.length)];
+    }
+
+}
